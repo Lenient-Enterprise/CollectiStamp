91199970e5f017ff3dd841924e3dcf91edcfbcfb
tcabgom <tcabgom222@gmail.com>
2023-11-18 14:22:22 +0100

    fix: Fixed images not being shown #6

--- collecti_stamp/product/models.py
+++ collecti_stamp/product/models.py
@@ -31,7 +31,7 @@ class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=4, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     stock_amount = models.IntegerField(validators=[
             MinValueValidator(limit_value=0, message='La cantidad no puede ser negativa.'),
             MaxValueValidator(limit_value=999, message='La cantidad no puede ser mayor que 999.'),
@@ -39,7 +39,7 @@ class Product(models.Model):
     product_type = models.CharField(max_length=10, choices=ProductType.choices)
     category = models.CharField(max_length=15, choices=Category.choices)
     criteria = models.ManyToManyField(Criteria)
-    image = models.ImageField(upload_to='static/img/product/', null=True, blank=True)
+    image = models.ImageField(upload_to='static/img/product/')
 
     class Meta:
         verbose_name = "Producto"
