7b1ec85cc1e9cf0b76e90c9461084f3aa278f8e7
Jesus Solis <jesussolisortega@gmail.com>
2023-11-18 13:12:54 +0100

    feat: avance parcial en catalogo

--- collecti_stamp/product/urls.py
+++ collecti_stamp/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import details_product
+from .views import details_product, product_catalog
 
 urlpatterns = [
     path('product/<int:product_id>/', details_product, name='detalle_producto'),
+    path('catalog/', product_catalog, name='product_catalog'),
 ]
 
