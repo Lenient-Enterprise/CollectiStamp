8f8fcd1967bf29ba7959458f37120a83aa2d101b
Jesus Solis <jesussolisortega@gmail.com>
2023-11-19 16:17:48 +0100

    feat: add about-us page

--- collecti_stamp/company/models.py
+++ collecti_stamp/company/models.py
@@ -1,3 +1,14 @@
+# company/models.py
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    contact_phone = models.TextField()
+    contact_email = models.TextField()
+    description = models.TextField()
+    logo = models.ImageField(upload_to='static/img/company/')
+
+    def __str__(self):
+        return self.name
+
