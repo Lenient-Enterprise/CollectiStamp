3ed4c701a5f1da69dbbcf7a79eb871032aec6d7b
Jesus Solis <jesussolisortega@gmail.com>
2023-11-19 19:15:55 +0100

    feat: update showcase

--- collecti_stamp/base/views.py
+++ collecti_stamp/base/views.py
@@ -4,8 +4,8 @@ from product.models import Product
 
 def home(request):
     # Obtén los tres productos más caros y los tres más baratos
-    expensive_products = Product.objects.all().order_by('-price')[:6]
-    cheap_products = Product.objects.all().order_by('price')[:6]
+    expensive_products = Product.objects.all().order_by('-price')[:3]
+    cheap_products = Product.objects.all().order_by('price')[:3]
 
     # Combina ambas listas para mostrar en la plantilla
     products = expensive_products | cheap_products
