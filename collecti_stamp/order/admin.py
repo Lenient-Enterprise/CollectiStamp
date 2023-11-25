43a664544ee1d6b3c3053d38fe0d25b4d6504b15
tcabgom <tcabgom222@gmail.com>
2023-11-25 16:27:25 +0100

    feat: Added Order class

--- collecti_stamp/order/admin.py
+++ collecti_stamp/order/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Order, OrderProduct
+
+
 # Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'order_is_finished', 'order_date', 'order_total', 'payment_method', 'delivery_status', 'delivery_method',)
+    search_fields = ('user_id',)
+    icon_name = 'shopping_cart'
+
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ('quantity',)
+    search_fields = ('order_id',)
+    icon_name = 'shopping_cart'
