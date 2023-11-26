61cb6389418efcb671c33e28bc906da228ef3513
alesanfe <alex.0002002@gmail.com>
2023-11-26 22:18:37 +0100

    fix: Intento para desplegar en la nube

--- collecti_stamp/base/views.py
+++ collecti_stamp/base/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 
-from collecti_stamp import settings
-
 
 def home(request):
-    print(settings.BASE_URL)
-
     return render(request, 'base/home.html')
--- collecti_stamp/collecti_stamp/settings.py
+++ collecti_stamp/collecti_stamp/settings.py
@@ -36,7 +36,6 @@ elif MODE == 'development':
     }
 
     CSRF_TRUSTED_ORIGINS = ['http://10.5.0.1:8000', 'http://localhost:8000']
-    BASE_URL = 'http://localhost:8000'
 
 
 
