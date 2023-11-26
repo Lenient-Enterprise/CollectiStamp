1f6f5ea3cd3089de8be5526fabb7726f23f78664
alesanfe <alex.0002002@gmail.com>
2023-11-26 17:58:40 +0100

    fix: Error en la configuración

--- collecti_stamp/collecti_stamp/deployment_settings.py
+++ collecti_stamp/collecti_stamp/deployment_settings.py
@@ -16,7 +16,7 @@ DATABASES = {
         'USER': os.environ.get('DB_USER', 'postgres'),
         'PASSWORD': os.environ.get('DB_PASSWORD', 'postgres'),
         'HOST': os.environ.get('DB_HOST', 'db'),
-        'PORT': int(os.environ.get('DB_PORT', 5432)),
+        'PORT': 5432,
     }
 }
 
