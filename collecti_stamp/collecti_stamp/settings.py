7ff83345bd18b3e99b6a54e8e6881444cd9bc2c5
alesanfe <alex.0002002@gmail.com>
2023-11-27 12:32:37 +0100

    fix: No funciona docker

--- collecti_stamp/collecti_stamp/settings.py
+++ collecti_stamp/collecti_stamp/settings.py
@@ -6,8 +6,8 @@ import dj_database_url
 
 # Access the environment variable MODE
 MODE = os.environ.get('MODE')
-
 ROOT_URLCONF = 'collecti_stamp.urls'
+DEBUG = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
