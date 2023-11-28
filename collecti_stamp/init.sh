05e74e8a19628d56f841008215733065ffd1ff6d
alesanfe <alex.0002002@gmail.com>
2023-11-28 19:12:46 +0100

    feat: Mejorado el init
    Ahora el init configura todo el proyecto para poder empezar directamente a programar como si hubieses hecho un clone.

--- collecti_stamp/init.sh
+++ collecti_stamp/init.sh
@@ -4,48 +4,84 @@
 
 BASE_DIR=$(dirname "$0")
 DB_FILE="$BASE_DIR/db.sqlite3"
+VENV_DIR="$BASE_DIR/venv"
+REQUIREMENTS_FILE="$BASE_DIR/requirements.txt"
 MIGRATIONS_DIR=$(find "$BASE_DIR" -type d -name migrations)
 JSON_DATA_FILE="$BASE_DIR/data/populate_user.json $BASE_DIR/data/populate_company_details.json $BASE_DIR/data/populate_criteria.json $BASE_DIR/data/populate_product.json"
 DELETE_DB=true
+
+DELETE_DB=false
+DELETE_ENV=false
 
 while [[ $# -gt 0 ]]; do
     key="$1"
 
     case $key in
-        -d|--delete-db)
+        -db|--delete-db)
             DELETE_DB=true
             shift
             ;;
+        -env|--delete-env)
+            DELETE_ENV=true
+            shift
+            ;;
         *)
-            # unknown option
-            echo "Unknown option: $key"
+            # Opción desconocida
+            echo "Opción desconocida: $key"
             exit 1
             ;;
     esac
 done
 
+
 reset_database() {
+    # Borrar el entorno virtual existente
+    if [ "$DELETE_ENV" == true ]; then
+        if [ -d "$VENV_DIR" ]; then
+            echo "Borrando el entorno virtual existente..."
+            rm -rf "$VENV_DIR"
+        fi
+
+        # Crear un nuevo entorno virtual
+        echo "Creando un nuevo entorno virtual..."
+        python3 -m venv "$VENV_DIR"
+
+        # Activar el entorno virtual
+        echo "Activando el entorno virtual..."
+        source "$VENV_DIR/bin/activate"
+
+        # Instalar las dependencias del archivo requirements.txt
+        echo "Instalando las dependencias del archivo requirements.txt..."
+        pip install -r "$REQUIREMENTS_FILE"
+        echo "----------------------------------------"
+    fi
+
+    # Configured template for commits
+    git config commit.template .gitmessage.txt
+    echo "Git commit template configured."
+    echo "----------------------------------------"
+
     # Deleting the existing db.sqlite3 file if DELETE_DB is true
     if [ "$DELETE_DB" == true ] && [ -e "$DB_FILE" ]; then
         rm "$DB_FILE"
         echo "Existing db.sqlite3 file deleted."
         echo "----------------------------------------"
-    fi
 
-    # Deleting migration files excluding __init__.py
-    for dir in $MIGRATIONS_DIR; do
-        if [ -e "$dir" ]; then
-            find "$dir" -type f ! -name "__init__.py" -exec rm {} \;
-            echo "Migration files in $dir deleted, excluding __init__.py."
+        # Deleting migration files excluding __init__.py
+        for dir in $MIGRATIONS_DIR; do
+            if [ -e "$dir" ]; then
+                find "$dir" -type f ! -name "__init__.py" ! -path "*/venv/*" -exec rm {} \;
+                echo "Migration files in $dir deleted, excluding __init__.py and files in paths containing '/venv/'."
+            fi
+        done
+        echo "----------------------------------------"
+
+        # Creating the db.sqlite3 file
+        if [ "$DELETE_DB" == true ]; then
+            touch "$DB_FILE"
+            echo "db.sqlite3 file created."
+            echo "----------------------------------------"
         fi
-    done
-    echo "----------------------------------------"
-
-    # Creating the db.sqlite3 file
-    if [ "$DELETE_DB" == true ]; then
-      touch "$DB_FILE"
-      echo "db.sqlite3 file created."
-      echo "----------------------------------------"
     fi
 
     # Updating migrations
@@ -71,5 +107,3 @@ reset_database() {
 }
 
 reset_database
-
-
