6d3f7072cf77f04ec2010350d24214cb560d9406
Ignarrman <nacho.arromant@gmail.com>
2023-12-01 20:09:46 +0100

    feat: Añadida separación por categorías #13

--- collecti_stamp/product/views.py
+++ collecti_stamp/product/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 from .models import Criteria
+from .models import Category
 
 
 def product_details(request, product_id):  # Change the parameter name
@@ -14,7 +15,15 @@ from django.db.models import Q
 def product_catalog(request):
     products = Product.objects.all()
     criteria = Criteria.objects.all()
-
+    categories= list(Product.objects.values('category').distinct().values_list('category', flat=True))
+    category_names = {
+        'ANCIENT': 'Antiguo',
+        'COMIC_BOOK': 'Comic',
+        'VIDEOGAME': 'Videojuego',
+        'HISTORICAL': 'Histórico',
+        'NOVELTY': 'Novedad',
+    }   
+    category_tuples = [(category, category_names.get(category, 'Desconocido')) for category in categories]
     criteria1 = request.GET.get('criteria1')
     criteria2 = request.GET.get('criteria2')
     criteria3 = request.GET.get('criteria3')
@@ -29,5 +38,5 @@ def product_catalog(request):
     coins=products.filter(product_type="COIN")
     seals=products.filter(product_type="SEAL")
      
-    return render(request, 'product/product_catalog.html', {'coins': coins, 'seals': seals, 'criteria': criteria})
+    return render(request, 'product/product_catalog.html', {'coins': coins, 'seals': seals, 'criteria': criteria, 'categories':category_tuples})
 
