c230022d667f3e82495170e68cf2230bb4125aa1
alesanfe <alex.0002002@gmail.com>
2023-12-02 00:16:14 +0100

    fix: Tarea cumple requisito

--- collecti_stamp/collecti_stamp/settings.py
+++ collecti_stamp/collecti_stamp/settings.py
@@ -122,8 +122,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alex.0002002@gmail.com'
-EMAIL_HOST_PASSWORD = 'whxj cvwg vxnv dybu'
+EMAIL_HOST_USER = 'lennient.enterprise@gmail.com'
+EMAIL_HOST_PASSWORD = 'yltf khuu xjdk vrah'
 
 # Admin site configuration
 MATERIAL_ADMIN_SITE = {
