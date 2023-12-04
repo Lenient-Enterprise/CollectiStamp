3da3345dfa307d3ce6c430b3c84e7fdeb72e0f4d
alesanfe <alex.0002002@gmail.com>
2023-12-04 20:59:10 +0100

    feat: Añadido listado

--- collecti_stamp/postorder/views.py
+++ collecti_stamp/postorder/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 
 from order.models import Order
 
 # Create your views here.
 def get_order_tracking(request, code):  # Change the parameter name
     order = get_object_or_404(Order, code=code)
-    return render(request, 'postorder/details.html', {'order': order})
+    return render(request, 'postorder/order_details.html', {'order': order})
+
+@login_required
+def list_order_tracking(request):
+    orders = Order.objects.filter(user_email=request.user.email)
+    return render(request, 'postorder/list.html', {'orders': orders})
