cd5142c47efb92e3f90db00ec5a5e9d82e59ee4b
Ignacio Gonzalez <igngongon2@alum.us.es>
2023-12-08 21:17:32 +0100

    fix: máximo de 10 productos en escaparate

--- collecti_stamp/base/views.py
+++ collecti_stamp/base/views.py
@@ -6,8 +6,8 @@ from product.models import Product
 @require_http_methods(["GET"])
 def home(request):
     # Obtén los tres productos más caros y los tres más baratos
-    coins = Product.objects.all().filter(product_type="COIN").order_by('price')
-    seals = Product.objects.all().filter(product_type="SEAL").order_by('price')
+    coins = Product.objects.all().filter(product_type="COIN").order_by('price')[:10]
+    seals = Product.objects.all().filter(product_type="SEAL").order_by('price')[:10]
 
     # Combina ambas listas para mostrar en la plantilla
     products = coins | seals
