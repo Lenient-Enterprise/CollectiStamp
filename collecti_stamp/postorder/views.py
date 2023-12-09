1a1f476a72e606a0f8c7371f410e7dfc257a706e
Ignarrman <nacho.arromant@gmail.com>
2023-12-09 19:55:09 +0100

    fix: Boton seguimiento

--- collecti_stamp/postorder/views.py
+++ collecti_stamp/postorder/views.py
@@ -1,5 +1,10 @@
+import re
+import uuid
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from flask import redirect
+from base.views import home
 
 from order.models import Order
 
@@ -25,28 +30,33 @@ def get_order_tracking(request, code):  # Change the parameter name
     payment_method=payment_methods.get(order.payment_method)
     return render(request, 'postorder/order_details.html', {'order': order, 'state':state,'delivery_method':delivery_method,'payment_method':payment_method})
 
+@require_http_methods(["GET"])
 def get_order_tracking_finder(request):  # Change the parameter name
     code = request.GET.get('code')
-    order = get_object_or_404(Order, code=code)
-    state_name = {
-        'IN_WAREHOUSE': 'En almacén',
-        'ON_THE_WAY': 'En camino',
-        'DELIVERED': 'Entregado',
-    }
-    delivery_methods = {
-        'STANDARD_SHIPPING': 'Envío estándar',
-        'EXPRESS_SHIPPING': 'Envío express',
-        'PICKUP_IN_STORE': 'Recogida en tienda',
-    }
-    payment_methods = {
-        'CASH_ON_DELIVERY': 'Contrarrembolso',
-        'PAYPAL': 'PayPal',
-    } 
-    state=state_name.get(order.delivery_status)
-    delivery_method=delivery_methods.get(order.delivery_method)
-    payment_method=payment_methods.get(order.payment_method)
-    return render(request, 'postorder/order_details.html', {'order': order, 'state':state,'delivery_method':delivery_method,'payment_method':payment_method})
-
+    guiones= re.sub(r'[^-]', '', code)
+    if (len(code) == 36) and (len(guiones)==4) and (Order.objects.filter(code=code).count()>0):
+        order= Order.objects.filter(code=code).first()
+        state_name = {
+            'IN_WAREHOUSE': 'En almacén',
+            'ON_THE_WAY': 'En camino',
+            'DELIVERED': 'Entregado',
+        }
+        delivery_methods = {
+            'STANDARD_SHIPPING': 'Envío estándar',
+            'EXPRESS_SHIPPING': 'Envío express',
+            'PICKUP_IN_STORE': 'Recogida en tienda',
+        }
+        payment_methods = {
+            'CASH_ON_DELIVERY': 'Contrarrembolso',
+            'PAYPAL': 'PayPal',
+        } 
+        state=state_name.get(order.delivery_status)
+        delivery_method=delivery_methods.get(order.delivery_method)
+        payment_method=payment_methods.get(order.payment_method)
+        return render(request, 'postorder/order_details.html', {'order': order, 'state':state,'delivery_method':delivery_method,'payment_method':payment_method})
+    else:
+        return home(request)
+    
 @login_required
 def list_order_tracking(request):
     orders = Order.objects.filter(user_email=request.user.email)
