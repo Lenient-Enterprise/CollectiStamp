4d95f5aefbf5240910b8731a06da7b78a1fcde1b
Alejandro Santiago Félix <72869496+alesanfe@users.noreply.github.com>
2023-12-09 17:22:36 +0100

    Update urls.py

--- collecti_stamp/collecti_stamp/urls.py
+++ collecti_stamp/collecti_stamp/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('base.urls')),
     path('catalog/', include('catalog.urls')),
     path('claim/', include('claim.urls')),
