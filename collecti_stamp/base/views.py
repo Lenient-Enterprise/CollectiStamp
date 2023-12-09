e1711c43f6b749c0dff655af5ef48fffe0abc090
Alejandro Santiago Félix <72869496+alesanfe@users.noreply.github.com>
2023-12-09 17:47:12 +0100

    fix: 10 productos

--- collecti_stamp/base/views.py
+++ collecti_stamp/base/views.py
@@ -7,23 +7,10 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 @require_http_methods(["GET"])
 def home(request):
     # Obtén los tres productos más caros y los tres más baratos
-    coins = Product.objects.all().filter(product_type="COIN").order_by('price')
-    seals = Product.objects.all().filter(product_type="SEAL").order_by('price')
+    coins = Product.objects.all().filter(product_type="COIN").order_by('price')[:9]
+    seals = Product.objects.all().filter(product_type="SEAL").order_by('price')[:9]
 
     # Combina ambas listas para mostrar en la plantilla
     products = coins | seals
-
-    # Cambio a 10 productos por página
-    paginator = Paginator(products, 20)  # Mostrar 20 productos por página
-    page = request.GET.get('page')
-
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        # Si la página no es un número entero, mostrar la primera página
-        products = paginator.page(1)
-    except EmptyPage:
-        # Si la página está fuera de rango (página que no existe), mostrar la última página
-        products = paginator.page(paginator.num_pages)
 
     return render(request, 'base/home.html', {'products': products})
