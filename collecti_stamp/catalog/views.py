7668b379f2e9607695c867a4eeeb6bbf1360f1c9
Ignarrman <nacho.arromant@gmail.com>
2023-12-12 19:57:43 +0100

    feet: Docker

--- collecti_stamp/catalog/views.py
+++ collecti_stamp/catalog/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.http import require_http_methods
 from product.models import Criteria, Product
 
 
+
 @require_http_methods(["GET"])
 def product_catalog(request):
     products = Product.objects.all()
@@ -67,14 +68,7 @@ def product_catalog_coins(request):
     criteria3 = request.GET.get('criteria3')
     product_type = request.GET.get('product_type')
     name = request.GET.get('name')
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        # Si la página no es un número entero, mostrar la primera página
-        products = paginator.page(1)
-    except EmptyPage:
-        # Si la página está fuera de rango (página que no existe), mostrar la última página
-        products = paginator.page(paginator.num_pages)
+
     if criteria1:
         products = products.filter(criteria__id=criteria1)
     if criteria2:
@@ -85,13 +79,18 @@ def product_catalog_coins(request):
         products = products.filter(product_type=product_type)
     if name:
         products = products.filter(name=name)
-
     products= products.filter(product_type='COIN')
-    # Cambio a 20 productos por página
+    
     paginator = Paginator(products, 20)  # Mostrar 20 productos por página
     page = request.GET.get('page')
-
-
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        # Si la página no es un número entero, mostrar la primera página
+        products = paginator.page(1)
+    except EmptyPage:
+        # Si la página está fuera de rango (página que no existe), mostrar la última página
+        products = paginator.page(paginator.num_pages)
 
     return render(request, 'product/product_catalog.html', {'products': products, 'criteria': criteria, 'categories': category_tuples})
 
